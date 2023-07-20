836f2451cbd45116824496ebd556fd651d08c00e
danielmegale <danim1@live.it>
2023-07-20 16:17:56 +0200

    added new task

--- api/index.php
+++ api/index.php
@@ -3,6 +3,18 @@ $database_path = __DIR__ . '../data/tasks.json';
 
 $json_data = file_get_contents($database_path);
 
-header('Content-Type: application/json');
+$tasks = json_decode($json_data, true);
 
-echo json_encode($tasks);
+$new_task = $_POST['task'] ?? null;
+if ($new_task) {
+    $tasks[] = $new_task;
+
+    $json_tasks = json_encode($tasks);
+    file_put_contents($database_path, $json_tasks);
+
+    header('Content-Type: application/json');
+    echo $new_task;
+} else {
+    header('Content-Type: application/json');
+    echo json_encode($tasks);
+}
--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const {createApp} =Vue;
 const app =createApp({
     data(){
         return{
-            tasks:[]
+            tasks:[],
+            newTask:'',
+        }
+    },
+    methods:{
+        addTask(){
+            const data = {task: this.newTask};
+            const config = {headers: {'Content-Type': 'multipart/form-data'}}
+
+            axios.post('http://localhost/php-todo-list-json/api/',data,config)  
+            .then(res => {
+                this.tasks.push(res.data);
+                this.newTask= '' ;
+            });
         }
     },
     created(){
